3aaf2d0a0a3350910d60779d10c4520bec7afd0b
Ravi kumar <56251535+imravi0009@users.noreply.github.com>
2020-08-02 19:50:09 +0530

    Initial commit

---
+++ Cavista-Assignment-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cavista-Assignment-iOS
+//
+//  Created by Ravi kumar on 02/08/20.
+//  Copyright © 2020 Ravi kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Cavista-Assignment-iOS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+ pod 'SnapKit'
+ pod 'Alamofire'
+  # Pods for Cavista-Assignment-iOS
+
+end
